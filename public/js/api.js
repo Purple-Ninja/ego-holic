f8aa03c0bfd163cc6ff4745efd3c1a64074ca3dd
Conrad Huang <wjhuang@yahoo-inc.com>
2016-12-15 00:41:20 +0800

    simplify the API fetch method

--- public/js/api.js
+++ public/js/api.js
@@ -1,26 +1,18 @@
 var API = (function() {
-    var url = 'http://52.198.213.105/getBestMoment.php?q=';
+    var url = 'http://localhost:9527/getBestMoment?q=';
 
     var fetch = function( query ) {
-        $.ajax({
-            url: url + query,
-            type: 'GET',
-            jsonpcallback: 'callback',
-            dataType: 'jsonp',
-            success: function( data ) {
+        $.get(url + query, function( data ) {
                 insertNewCard( data );
-            },
-            error: function( data ) {
-                insertNewCard( data.msg );
             }
-        });
+        );
     };
 
     var insertNewCard = function( data ) {
         console.log( data );
-    }
+    };
 
     return {
         fetch: fetch
-    }
+    };
 })();
