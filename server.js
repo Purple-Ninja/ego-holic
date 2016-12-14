b75cf38305ae44943aa55f3dca4b56a8a60b38a9
Maxis Kao <maxiskao@yahoo-inc.com>
2016-12-15 00:37:50 +0800

    add getBestMoment proxy

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 'use strict';
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const querystring = require('querystring');
+const _ = require('lodash');
+const port = process.env.PORT || 9527;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',function(req,res){
+app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-var port = process.env.PORT || 9527;
+app.get('/getBestMoment', (req, res) => {
+    var qs = querystring.stringify({ q: _.get(req, 'query.q', '') });
+    var reqUrl = 'http://52.198.213.105/getBestMoment.php?'+qs;
+    request(reqUrl, (err, response, body) => {
+        if (!err && response.statusCode == 200) {
+            res.json(JSON.parse(body));
+        } else {
+            res.end();
+        }
+    });
+});
 
-app.listen(port,function(){
+app.listen(port, () => {
     console.log('Listening on port', port);
 });
